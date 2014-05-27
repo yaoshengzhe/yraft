eb1e87a4af5b394e81eb1d9309540854ebbe8207
Shengzhe Yao <syao@rocketfuel.com>
2014-05-27 14:57:46 -0700

    draf a proof of concept Actor implementation

--- src/main/scala/com/github/yaoshengzhe/yraft/actor/ActorMain.scala
+++ src/main/scala/com/github/yaoshengzhe/yraft/actor/ActorMain.scala
@@ -1,4 +1,27 @@
-package com.github.yaoshengzhe.yraft.actor;
+package com.github.yaoshengzhe.yraft.actor
 
-public class ActorMain {
+import akka.actor.{Props, ActorSystem, Actor}
+
+class ServerActor extends Actor {
+
+  def receive = {
+    case "Hello" =>
+      println("World")
+  }
+}
+
+object ActorMain {
+  val actorSystem = ActorSystem("Yraft")
+  val serverActor = actorSystem.actorOf(Props[ServerActor])
+
+  def send (msg: String) {
+    serverActor ! msg
+    Thread.sleep(100)
+  }
+
+  def main(args: Array[String]) {
+    send("Hello")
+    send("Hello")
+    actorSystem.shutdown()
+  }
 }
