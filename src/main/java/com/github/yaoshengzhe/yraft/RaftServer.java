6f57ef65a2e94161833a1c2289d884a192c3bfa7
Shengzhe Yao <syao@rocketfuel.com>
2014-05-28 12:01:36 -0700

    Refactor RaftServer to make some methods public
    1. Make some event handle method in RaftServer public
    2. Remove main method from RaftServer
    3. Implement ServerActor and finish a working demo in ActorMain

--- src/main/java/com/github/yaoshengzhe/yraft/RaftServer.java
+++ src/main/java/com/github/yaoshengzhe/yraft/RaftServer.java
@@ -1,5 +1,6 @@
 package com.github.yaoshengzhe.yraft;
 
+import com.github.yaoshengzhe.yraft.network.Communicator;
 import com.github.yaoshengzhe.yraft.protobuf.generated.RaftProtos.AppendEntriesRequest;
 import com.github.yaoshengzhe.yraft.protobuf.generated.RaftProtos.AppendEntriesResponse;
 import com.github.yaoshengzhe.yraft.protobuf.generated.RaftProtos.LogEntry;
@@ -9,19 +10,28 @@ import com.github.yaoshengzhe.yraft.protobuf.generated.RaftProtos.VoteResponse;
 import com.github.yaoshengzhe.yraft.protobuf.generated.RaftProtos.VoteResponse.VoteDecision;
 import com.github.yaoshengzhe.yraft.statemachine.LocalDiskStateMachine;
 import com.github.yaoshengzhe.yraft.statemachine.StateMachine;
+import com.github.yaoshengzhe.yraft.timer.TimerService;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-import java.io.File;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-public class RaftServer {
+public class RaftServer implements Closeable {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   public static class Builder {
 
     private long candidateId;
     private StateMachine stateMachine;
+    private Communicator communicator;
+    private TimerService timerService;
 
     public Builder(long candidateId) {
       this.candidateId = candidateId;
@@ -29,6 +39,16 @@ public class RaftServer {
 
     public Builder setStateMachine(StateMachine stateMachine) {
       this.stateMachine = stateMachine;
+      return this;
+    }
+
+    public Builder setCommunicator(Communicator communicator) {
+      this.communicator = communicator;
+      return this;
+    }
+
+    public Builder setTimerService(TimerService timerService) {
+      this.timerService = timerService;
       return this;
     }
 
@@ -40,13 +60,23 @@ public class RaftServer {
   private RaftServer(Builder builder) {
     this.candidateId = builder.candidateId;
     this.stateMachine = builder.stateMachine;
+    this.communicator = builder.communicator;
+    this.timerService = builder.timerService;
+    this.timerService.init(new Runnable() {
+      @Override
+      public void run() {
+        onTimeout();
+      }
+    });
   }
 
   public static Builder newBuilder(long candidateId) {
     return new Builder(candidateId);
   }
 
-  private StateMachine stateMachine;
+  private final StateMachine stateMachine;
+  private final TimerService timerService;
+  private Communicator communicator;
 
   private Roles role = Roles.Follower;
 
@@ -79,22 +109,22 @@ public class RaftServer {
     this.role = role;
   }
 
-  private void onAppendEntriesRequest(AppendEntriesRequest request) {
+  public void onAppendEntriesRequest(AppendEntriesRequest request) {
 
   }
 
-  private void onAppendEntriesResponse(AppendEntriesResponse response) {
+  public void onAppendEntriesResponse(AppendEntriesResponse response) {
 
     if (response.getTerm() > this.currentTerm) {
       onTransition(Roles.Follower);
     }
   }
 
-  private void onVoteRequest(VoteRequest request) {
+  public void onVoteRequest(VoteRequest request) {
 
   }
 
-  private void onVoteResponse(VoteResponse response) {
+  public void onVoteResponse(VoteResponse response) {
     if (response.getTerm() > this.currentTerm) {
       onTransition(Roles.Follower);
     } else if (this.role == Roles.Candidate && response.getVoteDecision() == VoteDecision.GRANTED) {
@@ -102,7 +132,11 @@ public class RaftServer {
     }
   }
 
-  private void onTimeout() {
+  public synchronized void setCommunicator(Communicator communicator) {
+    this.communicator = communicator;
+  }
+
+  public void onTimeout() {
     switch (this.role) {
       case Follower:
         onTransition(Roles.Candidate);
@@ -112,6 +146,11 @@ public class RaftServer {
         startLeaderElection();
         break;
     }
+  }
+
+  @Override
+  public void close() throws IOException {
+    this.timerService.stop();
   }
 
   private void onTransition(Roles newRole) {
@@ -128,12 +167,25 @@ public class RaftServer {
   }
 
   private void startLeaderElection() {
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("Start Leader Election...");
+    }
+
+    // Should never happen
+    if (this.role != Roles.Candidate) {
+      throw new IllegalStateException("Your role is: " + this.role +
+              "! Do not have permission to start leader election.");
+    }
+
     VoteRequest request = VoteRequest.newBuilder()
             .setCandidateId(this.candidateId)
             .setLastLogIndex(this.getLastLogIndex())
             .setLastLogTerm(this.getLastLogTerm())
             .setTerm(this.currentTerm)
             .build();
+
+    this.communicator.send(Messages.VoteRequest, request.toByteArray());
     resetTimer();
   }
 
@@ -146,18 +198,11 @@ public class RaftServer {
   }
 
   private void resetTimer() {
-
+    this.timerService.reset();
   }
 
   public void run() {
     resetTimer();
   }
 
-  public static void main(String[] args) {
-    RaftServer node = RaftServer.newBuilder(1)
-            .setStateMachine(new LocalDiskStateMachine(new File("")))
-            .build();
-
-    node.run();
-  }
 }
--- src/main/scala/com/github/yaoshengzhe/yraft/actor/ActorMain.scala
+++ src/main/scala/com/github/yaoshengzhe/yraft/actor/ActorMain.scala
@@ -1,27 +1,65 @@
 package com.github.yaoshengzhe.yraft.actor
 
 import akka.actor.{Props, ActorSystem, Actor}
+import com.github.yaoshengzhe.yraft.statemachine.LocalDiskStateMachine
+import java.io.File
+import com.github.yaoshengzhe.yraft.network.ActorBasedCommunicator
+import com.github.yaoshengzhe.yraft.{Messages, RaftServer}
+import com.github.yaoshengzhe.yraft.protobuf.generated.RaftProtos.{AppendEntriesResponse, AppendEntriesRequest, VoteResponse, VoteRequest}
+import com.github.yaoshengzhe.yraft.exception.UnknownRaftMessageException
+import com.typesafe.scalalogging.slf4j.Logger
+import org.slf4j.LoggerFactory
+import com.github.yaoshengzhe.yraft.timer.ScheduledExecutorTimerService
+import java.util.concurrent.TimeUnit
 
-class ServerActor extends Actor {
+class ServerActor(server: RaftServer) extends Actor {
+
+  val logger = Logger(LoggerFactory getLogger this.getClass.getSimpleName)
 
   def receive = {
-    case "Hello" =>
-      println("World")
+    case (msg: Messages, data: Array[Byte]) =>
+      handle(msg, data)
+    case _ =>
+      throw new UnknownRaftMessageException("Unknown message.")
+  }
+
+  def handle(msg: Messages, data: Array[Byte]): Unit = {
+    msg match {
+      case Messages.VoteRequest =>
+        logger.debug("Received VoteRequest: " + VoteRequest.parseFrom(data))
+        this.server.onVoteRequest(VoteRequest.parseFrom(data))
+      case Messages.VoteResponse =>
+        this.server.onVoteResponse(VoteResponse.parseFrom(data))
+      case Messages.AppendEntriesRequest =>
+        this.server.onAppendEntriesRequest(AppendEntriesRequest.parseFrom(data))
+      case Messages.AppendEntriesResponse =>
+        this.server.onAppendEntriesResponse(AppendEntriesResponse.parseFrom(data))
+      case _ =>
+        throw new UnknownRaftMessageException("Unknown message: " + msg)
+    }
   }
 }
 
 object ActorMain {
-  val actorSystem = ActorSystem("Yraft")
-  val serverActor = actorSystem.actorOf(Props[ServerActor])
 
-  def send (msg: String) {
-    serverActor ! msg
-    Thread.sleep(100)
+  def newServer(commitFilePath: String, actorSystem: ActorSystem): RaftServer = {
+
+    val server: RaftServer = RaftServer.newBuilder
+      .setStateMachine(new LocalDiskStateMachine(new File(commitFilePath)))
+      .setTimerService(new ScheduledExecutorTimerService(150, TimeUnit.MILLISECONDS))
+      .build()
+
+    val serverActor = actorSystem.actorOf(Props(classOf[ServerActor], server))
+    // Then set communicator, order is important here:)
+    server.setCommunicator(new ActorBasedCommunicator(serverActor))
+    server
   }
 
   def main(args: Array[String]) {
-    send("Hello")
-    send("Hello")
-    actorSystem.shutdown()
+
+    val actorSystem = ActorSystem("YRaft")
+    val commitFilePath = "~/tmp/raft"
+
+    (0 to 10).foreach(i => newServer(commitFilePath + i, actorSystem).run())
   }
 }
