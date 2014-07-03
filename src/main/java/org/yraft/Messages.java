46fe1987ba6506d535d0a917ef4b0ae0b32aeaa6
Shengzhe Yao <yaoshengzhe@gmail.com>
2014-07-02 21:35:06 -0700

    add id field for Messages

--- src/main/java/org/yraft/Messages.java
+++ src/main/java/org/yraft/Messages.java
@@ -1,9 +1,19 @@
 package org.yraft;
 
 public enum Messages {
-  VoteRequest,
-  VoteResponse,
-  AppendEntriesRequest,
-  AppendEntriesResponse,
-  HeartBeat
+  VoteRequest(1),
+  VoteResponse(2),
+  AppendEntriesRequest(3),
+  AppendEntriesResponse(4),
+  HeartBeat(5);
+
+  private final int id;
+
+  private Messages(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return this.id;
+  }
 }
